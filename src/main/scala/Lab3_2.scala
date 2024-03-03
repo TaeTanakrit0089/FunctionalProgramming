e74fac17f35b07e209c4b1bc769ba7ec6f7460bc
tae13152 <65070089@kmitl.ac.th>
2024-03-03 19:55:45 +0700

    Added Lab3

--- src/main/scala/Lab3_2.scala
+++ src/main/scala/Lab3_2.scala
@@ -18,5 +18,37 @@ object Lab3_2 {
     val jsonString = os.read(os.pwd / "full.json")
     val data = ujson.read(jsonString)
     println(data(0)("id"))
+
+    println(output(0))
+    println(output(0)("hello"))
+
+    val small = ujson.Arr(
+      ujson.Obj("hello" -> "world", "answer" -> 42),
+      true
+    )
+    println(small)
+    small(0)("hello") = "goodbye"
+    println(small)
+
+    small(0)("tags") = ujson.Arr("awesome", "yay", "wonderful")
+
+    println(small)
+    small(0).obj.remove("hello")
+    small.arr.append(123)
+
+    println(small)
+
+    def traverse(v: ujson.Value): Iterable[String] = v match {
+      case a: ujson.Arr => a.arr.map(traverse).flatten
+      case o: ujson.Obj => o.obj.values.map(traverse).flatten
+      case s: ujson.Str => Seq(s.str)
+      case _ => Nil
+    }
+
+//    println(traverse(small))
+//    println(traverse(output))
+    println(traverse(data))
+
+
   }
 }
