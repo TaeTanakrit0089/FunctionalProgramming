477536c10d81dfd635fde2db24955d7938ffc846
tae13152 <65070089@kmitl.ac.th>
2024-03-13 14:52:20 +0700

    Added Java11

--- src/main/scala/sparkscala.scala
+++ src/main/scala/sparkscala.scala
@@ -8,7 +8,24 @@ object sparkscala {
       .appName("sparktest")
       .getOrCreate()
 
-    //code here
+    // Read files from multiple directories into Single RDD
+    val rdd2 = spark.sparkContext.textFile("data/text01.txt,data/text02.txt,data/text03.txt")
+    rdd2.foreach(f => println(f))
+
+    // Read from csv file into RDD
+    val rddFromFile = spark.sparkContext.textFile("data/AAPL.csv")
+
+    // Applying map() on Rdd to get array[string]
+    val rdd3 = rddFromFile.map(f => {
+      f.split(",")
+    })
+
+    // Read data from RDD using foreach
+    rdd3.foreach(f => {
+      println("Col1:" + f(0) + ",Col2:" + f(1))
+    })
+
+
     val rdd: RDD[Int] = spark.sparkContext.parallelize(List(1, 2, 3, 4, 5))
     val rddCollect: Array[Int] = rdd.collect()
     println("Number of Partitions: " + rdd.getNumPartitions)
