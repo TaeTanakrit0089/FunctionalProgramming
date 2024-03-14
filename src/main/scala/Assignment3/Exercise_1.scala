baaee44e7cc4854c3fd4027a17564f4688d976df
tae13152 <65070089@kmitl.ac.th>
2024-03-14 18:20:06 +0700

    Added Assignment 3

--- src/main/scala/Assignment3/Exercise_1.scala
+++ src/main/scala/Assignment3/Exercise_1.scala
@@ -7,29 +7,30 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
 
 import java.io.File
 
-case class Product(name: String, url: String, image: String, price: String)
+case class Result(name: String, image: String, timestamp: String)
 
 object Exercise_1 {
   def main(args: Array[String]): Unit = {
     // initialize the Jsoup-backed browser
     val browser = JsoupBrowser()
     // download the target page
-    val doc = browser.get("https://www.it.kmitl.ac.th/~it65070089/Lobsters.html")
+    val doc = browser.get("https://www.it.kmitl.ac.th/~it65070089/works/Lobsters.html")
 
     // get the first HTML product on the page
-    val htmlProductElements = doc >> elementList("li.product")
+    val htmlProductElements = doc >> elementList("div.byline")
+
+    println(htmlProductElements >> element("img"))
 
     // transform the HTML product elements into a list
     // of Product instances
-    val products: List[Product] = htmlProductElements.map(htmlProductElement => {
+    val result: List[Result] = htmlProductElements.map(htmlProductElement => {
       // extract the desired data from it
-      val name = htmlProductElement >> text("h2")
-      val url = (htmlProductElement >> element("a")).attr("href")
+      val name = (htmlProductElement >> element("img")).attr("alt")
       val image = (htmlProductElement >> element("img")).attr("src")
-      val price = htmlProductElement >> text("span")
+      val timestamp = htmlProductElement >> text("span")
 
       // return a new Product instance
-      Product(name, url, image, price)
+      Result(name, image, timestamp)
     })
 
     // create the output file
@@ -38,7 +39,7 @@ object Exercise_1 {
     val writer = CSVWriter.open(outputFile)
     // transform the products in the format required by the
     // writer and populate the CSV output file
-    writer.writeAll(products.map(product => List(product.name, product.url, product.image, product.price)))
+    writer.writeAll(result.map(result_member => List(result_member.name, result_member.image, result_member.timestamp)))
     // release the writer resources
     writer.close()
   }
--- src/main/scala/Assignment3/ScalaScraper.scala
+++ src/main/scala/Assignment3/ScalaScraper.scala
@@ -15,6 +15,8 @@ object ScalaScraper {
     val browser = JsoupBrowser()
     // download the target page
     val doc = browser.get("https://scrapeme.live/shop/")
+    println(doc)
+//    return
 
     // get the first HTML product on the page
     val htmlProductElements = doc >> elementList("li.product")
