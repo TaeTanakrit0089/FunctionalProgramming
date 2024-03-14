1c18cdece24a18858af4ddfd8eee16e1b7d2f677
tae13152 <65070089@kmitl.ac.th>
2024-03-14 22:12:34 +0700

    Added Complete Assignment 3

--- src/main/scala/Assignment3/Exercise_2.scala
+++ src/main/scala/Assignment3/Exercise_2.scala
@@ -10,21 +10,20 @@ import java.io.File
 class Student(val std_id: String, val participation: String, val score: String)
 
 object Exercise_2 {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     val browser = JsoupBrowser()
-    val doc = browser.get("https://www.it.kmitl.ac.th/~pattarachai/DiscreteMath/dm.htm")
+    val doc = browser.get("https://www.it.kmitl.ac.th/~pattarachai/DiscreteMath/dm.htm") // ดึง HTML มาจากเว็ป
 
-    val htmlProductElements = doc >> elementList("tr")
-    val data = htmlProductElements.drop(6)
+    val htmlProductElements = doc >> elementList("tr") // เอามาเฉพาะ tr และเอามาทำเป็น list
+    val data = htmlProductElements.drop(6) // ลบหัวเรื่องของตารางออก
 
     val student_result: List[Student] = data.map({ x =>
-      val tdElements = x >> elementList("td")
-      Student(tdElements.head.text, tdElements(1).text, tdElements(2).text)
+      val tdElements = x >> elementList("td") // เอา td ทุกตัวใน tr มาทำเป็น list
+      Student(tdElements.head.text, tdElements(1).text, tdElements(2).text) // ในตารางมีอยู่ 3 คอลัมน์เลยแตกตามนี้
     })
 
-    val outputFile = new File("exercise2.csv")
+    val outputFile = new File("exercise2.csv") // สร้างไฟล์ csv
     val writer = CSVWriter.open(outputFile)
-    writer.writeAll(student_result.map(x => List(x.std_id, x.participation, x.score)))
+    writer.writeAll(student_result.map(x => List(x.std_id, x.participation, x.score))) // แปลงให้เป็น list อีกรอบนึง
     writer.close()
-  }
 }
