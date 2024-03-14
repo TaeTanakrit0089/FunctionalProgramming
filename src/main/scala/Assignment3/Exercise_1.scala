9713b36daf373510273364d1ea9a981aa40a31cf
tae13152 <65070089@kmitl.ac.th>
2024-03-14 22:15:16 +0700

    Added Complete Assignment 3

--- src/main/scala/Assignment3/Exercise_1.scala
+++ src/main/scala/Assignment3/Exercise_1.scala
@@ -11,37 +11,22 @@ case class Result(name: String, image: String, timestamp: String)
 
 object Exercise_1 {
   def main(args: Array[String]): Unit = {
-    // initialize the Jsoup-backed browser
     val browser = JsoupBrowser()
-    // download the target page
-    val doc = browser.get("https://www.it.kmitl.ac.th/~it65070089/works/Lobsters.html")
+    val doc = browser.get("https://www.it.kmitl.ac.th/~it65070089/works/Lobsters.html") // ดึงข้อมูล Lobster.html ออกมา
 
-    // get the first HTML product on the page
-    val htmlProductElements = doc >> elementList("div.byline")
+    val htmlProductElements = doc >> elementList("div.byline") // แตก div ที่มีคลาส byline
 
-    println(htmlProductElements >> element("img"))
-
-    // transform the HTML product elements into a list
-    // of Product instances
     val result: List[Result] = htmlProductElements.map(htmlProductElement => {
-      // extract the desired data from it
-      val name = (htmlProductElement >> element("img")).attr("alt")
-      val image = (htmlProductElement >> element("img")).attr("src")
-      val timestamp = htmlProductElement >> text("span")
+      val name = (htmlProductElement >> element("img")).attr("alt") // เอาค่า alt ของ img มา
+      val image = (htmlProductElement >> element("img")).attr("src") // เอาค่า src ของ img มา
+      val timestamp = htmlProductElement >> text("span") // เอาค่า alt ของ img มา
 
-      println(name + ", " + image + ", " + timestamp)
-      // return a new Product instance
-      Result(name, image, timestamp)
+      Result(name, image, timestamp) // ส่งต่า
     })
 
-    // create the output file
     val outputFile = new File("exercise1.csv")
-    // initialize the CSV writer
     val writer = CSVWriter.open(outputFile)
-    // transform the products in the format required by the
-    // writer and populate the CSV output file
     writer.writeAll(result.map(result_member => List(result_member.name, result_member.image, result_member.timestamp)))
-    // release the writer resources
     writer.close()
   }
 }
